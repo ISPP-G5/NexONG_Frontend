674eca8d933ca7bd6cd20de7eba6e4efe68ed4d1
claugp06 <99924348+claugp06@users.noreply.github.com>
2024-02-22 21:05:00 +0100

    Feat:HomePage

--- src/screens/App.js
+++ src/screens/App.js
@@ -1,58 +1,36 @@
+
+import Donation from './Donation';
+import Suggestions from './Suggestions';
+import  Register from './Register';
+import LogIn from './LogIn';
+import Camps from './camps';
+import CoexistanceRoom from './CoexistanceRoom';
+import FamilyWorkshop from './FamilyWorkshops';
+import OpenRoom from './openRoom';
+import SummerClub from './summerClub';
 import { BrowserRouter as Router, Route , Routes} from 'react-router-dom';
 import '../styles/styles.css';
-
-
+import HomePage from './HomePage';
 function App() {
   return (
-    <div className="App">
-      <div className='header'>
-        <div>
-          <ul><li>
-            <a>Actividades</a>
-            <ul>
-              <li><a>Campamentos</a></li>
-              <li><a>Clases</a></li>
-            </ul>
-          </li></ul>
-        </div>
-        <a>Donaciones</a>
-        <a>Sugerencias</a>
-        <a>Registrase</a>
-        <a>Iniciar Sesión</a>
-      </div>
-      <div className='main'>
-      <div className='title-text'>Esto es un Título</div>
-      <div className='text'>Esto es un ejemplo de texto para la información 
-      de las pantallas de la HomePage</div>
+    <Router>
+      <Routes>
+     
+            <Route path="/" element={<HomePage />} />
+            <Route path="/donaciones" element={<Donation />} />
+            <Route path="/sugerencias" element={<Suggestions />} />
+            <Route path="/registrarse" element={<Register />} />
+            <Route path="/iniciar-sesion" element={<LogIn />} />
+            <Route path="/aula-abierta" element={<OpenRoom />} />
+            <Route path="/campamentos" element={<Camps />} />
+            <Route path="/aula-convivencia" element={<CoexistanceRoom />} />
+            <Route path="/talleres-familiares" element={<FamilyWorkshop />} />
+            <Route path="/club-verano" element={<SummerClub />} />
 
-      <div className='flex-container'>
-        <div className='field-text'>Nombre</div>
-        <input type='text' placeholder='Name'></input>
-        <input type='text' placeholder='Name'></input>
-        <input type='text' placeholder='Name'></input>
-        <select placeholder='Name'>
-          <option>campamentos</option>
-          <option>actividades</option>
-        </select>
-        <button className='button'>Enviar</button>
-      </div>
-    </div>
-
-    <div className='menu'>
-      <a>Voluntarios</a>
-      <a>Educadores</a>
-      <a>Familias</a>
-    </div>
-
-    <div className='footer'>
-    <div className='header-text'>Actividades</div>
-        <div className='header-text'>Donaciones</div>
-        <div className='header-text'>Sugerencias</div>
-        <div className='header-text'>Registrase</div>
-        <div className='header-text'>Inciar Sesión</div>
-    </div>
-    </div>
+            </Routes>
+    </Router>
+    
+    
   );
 }
-
 export default App;
---
+++ src/screens/camps.js
@@ -0,0 +1,78 @@
+import logo from '../logo/macn-logo.png';
+import InboxIcon from '@material-ui/icons/Inbox';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import WhatsAppIcon from '@material-ui/icons/WhatsApp';
+import {Link} from 'react-router-dom';
+import '../styles/styles.css';
+//Footer and header include it.
+function Camps() {
+        return (
+        <div className="App">
+        <div className='header'>
+            <div>
+            <img src={logo} alt="Logo" className="header-logo" />
+
+            <ul><li>
+                <Link to="/actividades">Actividades</Link> {/* Usa el componente Link para crear enlaces a las rutas */}
+                <ul>
+                <li><Link to="/campamentos">Campamentos</Link></li>
+                <li><Link to="/aula-abierta">Aula abierta</Link></li>
+                <li><Link to="/aula-convivencia">Aula de convivencia</Link></li>
+                <li><Link to="/talleres-familiares">Talleres familiares</Link></li>
+                <li><Link to="/club-verano">Club de verano</Link></li>
+
+                </ul>
+            </li></ul>
+            </div>
+            <Link to="/donaciones">Donaciones</Link>
+            <Link to="/sugerencias">
+            Sugerencias
+            <InboxIcon style={{ marginLeft: '10px',textAlign: 'c' }} />
+            </Link>
+            <Link to="/registrarse">Registrase</Link>
+            <Link to="/iniciar-sesion">Iniciar Sesión</Link>
+
+
+        </div>
+        <div className='main'>
+            
+
+            
+        </div>
+        <div className='footer'>
+        <div className='header-text'>
+            Email: <a href="mailto:manosabiertas@lasalleandalucia.net" style={{ color: 'black' }}>manosabiertas@lasalleandalucia.net</a>
+            </div>
+        <div className='header-text'>Polígono norte Sevilla (41009):</div>
+        <div className='header-text'>
+        <a href="https://www.facebook.com/ajmmaa/" target="_blank" rel="noopener noreferrer">
+        <FacebookIcon style={{ color: '#3b5998' }} />
+        </a>
+        <a href="https://twitter.com/ajmmaa" target="_blank" rel="noopener noreferrer">
+            <TwitterIcon style={{ color: '#1DA1F2' }} />
+        </a>
+        <a href="https://www.instagram.com/ajmanosabiertas/" target="_blank" rel="noopener noreferrer">
+            <InstagramIcon style={{ color: '#C13584' }} />
+        </a> 
+        <a href="https://api.whatsapp.com/send/?phone=650485214&text&type=phone_number&app_absent=0" target="_blank" rel="noopener noreferrer">
+            <WhatsAppIcon style={{ color: '#25D366' }} />
+        </a>  
+        </div>
+        <div className='header-text'>Teléfonos:<br />650485214 <br />640168593</div>
+        <div className='header-text'>
+            Plaza Río de Janeiro, 10 <br />
+            <span className="indented-text">Calle Meléndez Valdés, 28</span>
+        </div>
+        <div className='header-text'></div>
+        <div className='header-text'>Fax:158 425 252</div>
+        </div>
+
+        
+        </div>
+        
+
+        );
+    }
+export default Camps;    
