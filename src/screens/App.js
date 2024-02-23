c9e1f181fcd87f2c90e5b78da1f049c523264042
fracalrod3 <fracalrod3@alum.us.es>
2024-02-23 17:38:14 +0100

    feat: Admin screen-Account configuration

--- src/screens/App.js
+++ src/screens/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router, Route , Routes} from 'react-router-dom';
 import '../styles/styles.css';
+import AdminProfileUpdate from './admin/AdminProfileUpdate';
+import AdminProfile from './admin/AdminProfile';
+
 
 
 function App() {
   return (
-    <div className="App">
+    <><div className="App">
       <div className='header'>
         <div>
           <ul><li>
@@ -21,37 +24,42 @@ function App() {
         <a>Iniciar Sesión</a>
       </div>
       <div className='main'>
-      <div className='title-text'>Esto es un Título</div>
-      <div className='text'>Esto es un ejemplo de texto para la información 
-      de las pantallas de la HomePage</div>
+        <div className='title-text'>Esto es un Título</div>
+        <div className='text'>Esto es un ejemplo de texto para la información
+          de las pantallas de la HomePage</div>
 
-      <div className='flex-container'>
-        <div className='field-text'>Nombre</div>
-        <input type='text' placeholder='Name'></input>
-        <input type='text' placeholder='Name'></input>
-        <input type='text' placeholder='Name'></input>
-        <select placeholder='Name'>
-          <option>campamentos</option>
-          <option>actividades</option>
-        </select>
-        <button className='button'>Enviar</button>
+        <div className='flex-container'>
+          <div className='field-text'>Nombre</div>
+          <input type='text' placeholder='Name'></input>
+          <input type='text' placeholder='Name'></input>
+          <input type='text' placeholder='Name'></input>
+          <select placeholder='Name'>
+            <option>campamentos</option>
+            <option>actividades</option>
+          </select>
+          <button className='button'>Enviar</button>
+        </div>
       </div>
-    </div>
 
-    <div className='menu'>
-      <a>Voluntarios</a>
-      <a>Educadores</a>
-      <a>Familias</a>
-    </div>
+      <div className='menu'>
+        <a>Voluntarios</a>
+        <a>Educadores</a>
+        <a>Familias</a>
+      </div>
 
-    <div className='footer'>
-    <div className='header-text'>Actividades</div>
+      <div className='footer'>
+        <div className='header-text'>Actividades</div>
         <div className='header-text'>Donaciones</div>
         <div className='header-text'>Sugerencias</div>
         <div className='header-text'>Registrase</div>
         <div className='header-text'>Inciar Sesión</div>
-    </div>
-    </div>
+      </div>
+    </div><Router>
+        <Routes>
+          <Route path="/adminProfile" exact={true} element={<AdminProfile />} />
+          <Route path="/adminProfileUpdate" exact={true} element={<AdminProfileUpdate />} />
+        </Routes>
+      </Router></>
   );
 }
 
---
+++ src/screens/admin/AdminProfileUpdate.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import '../../styles/styles.css'
+
+const adData = [{id: 1, nombre: "Simiente", email: "simtofe@manosabiertas.com", password: "cimientos", telefono: 609123324, direccion: "C. Manzana 11, 41009, Sevilla", imagen: "https://covalto-production-website.s3.amazonaws.com/Hero_Mobile_Cuenta_Personas_V1_1_8046e424ea.webp" }]
+
+const AdminProfilesUpdate = () => {
+    return (
+      <>
+        <ul>
+          {adData.map(v => {
+            return (
+                <div style={{ border: ' 5px outset grey', width: '50%', borderRadius: '20%', height: '85%', marginLeft: '30%'}}>
+                <div style={{textAlign:'center', margin:'5%'}}> 
+                  <img src={v.imagen} alt={"imagen de" + v.nombre} style={{ 
+                      maxWidth: '60%', 
+                      maxHeight: '60%', 
+                      borderRadius: '100%',
+                    }}  />
+                </div>
+                <div style={{textAlign:'center'}}>
+                    <div className='field-text' style={{ textAlign: 'center', fontWeight: 'bold'}}
+                        >{v.nombre}</div>
+                </div>
+                    <div className='field-text'
+                    style={{ fontWeight: 'bold', marginLeft: '10%'}}
+                    >Email</div>
+                        <input type='text' placeholder='Email' style={{margin: '10px', maxWidth: '80%', marginLeft: '10%'}}></input>
+
+                    <div className='field-text'
+                    style={{ fontWeight: 'bold', marginLeft: '10%'}}
+                    >Teléfono</div>
+                        <input type='text' placeholder='Número de teléfono' style={{margin: '10px', maxWidth: '80%', marginLeft: '10%'}}></input>
+
+                    <div className='field-text'
+                    style={{ fontWeight: 'bold', marginLeft: '10%'}}
+                    >Contraseña</div>
+                        <input type='text' placeholder='Contraseña' style={{margin: '10px', maxWidth: '80%', marginLeft: '10%'}}></input>
+
+                    <div className='field-text'
+                    style={{ fontWeight: 'bold', marginLeft: '10%'}}
+                    >Dirección</div>
+                        <input type='text' placeholder='Dirección' style={{margin: '10px', maxWidth: '80%', marginLeft: '10%'}}></input>
+                    <div style={{textAlign:'center'}}>
+                    <button className='button'
+                        style={{ backgroundColor: 'aed6f1', maxHeight: '60%', margin:'4%'}}>
+                            Actualizar perfil
+                    </button></div>
+                </div>
+              )
+          }
+          )
+          }
+        </ul>
+      </>
+  
+    )
+  
+  };
+  
+  export default AdminProfilesUpdate;
