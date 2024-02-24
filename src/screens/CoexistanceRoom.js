073102b534629ad56536c04c12885d34d347bad1
FranciscoRossoR <franrosora@gmail.com>
2024-02-24 15:33:20 +0100

    Finish CoexistanceRoom screen

--- src/screens/CoexistanceRoom.js
+++ src/screens/CoexistanceRoom.js
@@ -1,8 +1,31 @@
+import '../styles/styles.css';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
 function CoexistanceRoom() {
     return (
-      <div>
-        <h1>Aula convivencia</h1>
-      </div>
+        <div className="App">
+
+            <Header/>
+
+            <div className='main'>
+                <div className='title-text'>AULA DE CONVIVENCIA</div>
+                <div className='flex-container-blue'>
+                    Atención a alumnos con conductas disruptivas para menores entre
+                    6 y 18 años así como actividades de relajacion y reflexión
+                </div>
+                <div className='text'>
+                    Lunes a jueves
+                    <br/>
+                    9:00 a 14:00
+                </div>
+            </div>
+
+            <Footer/>
+
+        </div>
+        
+
     );
-  }
-  export default CoexistanceRoom;
+}
+export default CoexistanceRoom;
