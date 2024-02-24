d903fb4a3436c36ed284d6b9f0139c81b7e77699
FranciscoRossoR <franrosora@gmail.com>
2024-02-24 15:34:10 +0100

    Finish FamilyWorkshops screen

--- src/screens/FamilyWorkshops.js
+++ src/screens/FamilyWorkshops.js
@@ -1,8 +1,28 @@
-function FamilyWorkshop() {
+import '../styles/styles.css';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+function FamilyWorkshops() {
     return (
-      <div>
-        <h1>Talleres en familia </h1>
-      </div>
+        <div className="App">
+
+            <Header/>
+
+            <div className='main'>
+                <div className='title-text'>TALLERES FAMILIARES</div>
+                <div className='flex-container-blue'>
+                    Actividades formativas y de poyo para las familias
+                </div>
+                <div className='text'>
+                    Sesiones quincenales
+                </div>
+            </div>
+
+            <Footer/>
+
+        </div>
+        
+
     );
-  }
-  export default FamilyWorkshop;
+}
+export default FamilyWorkshops;
