a649a97b874f7ca7e0394164f367a8664399348b
FranciscoRossoR <franrosora@gmail.com>
2024-02-24 15:35:03 +0100

    Finish SummerClub screen

--- src/screens/SummerClub.js
+++ src/screens/SummerClub.js
@@ -1,8 +1,29 @@
+import '../styles/styles.css';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
 function SummerClub() {
     return (
-      <div>
-        <h1>Club de verano</h1>
-      </div>
+        <div className="App">
+
+            <Header/>
+
+            <div className='main'>
+                <div className='title-text'>CLUB DE VERANO</div>
+                <div className='flex-container-blue'>
+                    Club de verano para alumnos de primaria 
+                </div>
+                <div className='text'>
+                    Ven y disfruta de actividades como manualidades,
+                    educación en valores y mucho más
+                </div>
+            </div>
+
+            <Footer/>
+
+        </div>
+        
+
     );
-  }
-  export default SummerClub;
+}
+export default SummerClub;
