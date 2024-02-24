e6f8d344250f59dedb992d34f5078fd62aa2a2f5
FranciscoRossoR <franrosora@gmail.com>
2024-02-24 15:34:29 +0100

    Finish OpenRoom screen

--- src/screens/OpenRoom.js
+++ src/screens/OpenRoom.js
@@ -1,8 +1,31 @@
+import '../styles/styles.css';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
 function OpenRoom() {
     return (
-      <div>
-        <h1>Aula abierta</h1>
-      </div>
+        <div className="App">
+
+            <Header/>
+
+            <div className='main'>
+                <div className='title-text'>AULA ABIERTA</div>
+                <div className='flex-container-blue'>
+                    Refuerzo educativo para menores entre 6 y 18 años así
+                    como actividades de relajacion y reflexión
+                </div>
+                <div className='text'>
+                    Lunes a jueves
+                    <br/>
+                    16:00 a 20:00
+                </div>
+            </div>
+
+            <Footer/>
+
+        </div>
+        
+
     );
-  }
-  export default OpenRoom;
+}
+export default OpenRoom;
