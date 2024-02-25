57b440006e3b923ff4b64f5ee29b2e95e1c87185
fracalrod3 <fracalrod3@alum.us.es>
2024-02-25 17:13:24 +0100

    Reverting to ehn codacy liked me

--- src/screens/App.js
+++ src/screens/App.js
@@ -14,7 +14,6 @@ import '../styles/styles.css';
 import AdminProfileUpdate from './AdminProfileUpdate';
 import AdminProfile from './AdminProfile';
 import HomePage from './HomePage';
-import VolunteerForm from './VolunteerForm';
 
 function App() {
   return (
@@ -28,7 +27,6 @@ function App() {
             <Route path="/iniciar-sesion" element={<LogIn />} />
             <Route path="/aula-abierta" element={<OpenRoom />} />
             <Route path="/campamentos" element={<Camps />} />
-            <Route path="/form-voluntario" element={<VolunteerForm />} />
             <Route path="/aula-convivencia" element={<CoexistanceRoom />} />
             <Route path="/talleres-familiares" element={<FamilyWorkshop />} />
             <Route path="/club-verano" element={<SummerClub />} />
