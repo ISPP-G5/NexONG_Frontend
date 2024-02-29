e25379e0777d11107cb659776e255dcb3889827f
Maria Nuñez <marnunrey2@alum.us.es>
2024-02-29 23:28:30 +0100

    fix: codacy problems

--- src/screens/AdminFamily.js
+++ src/screens/AdminFamily.js
@@ -1,7 +1,33 @@
-import { BrowserRouter as Link} from 'react-router-dom';
+import { Link} from 'react-router-dom';
 import '../styles/admin-family.css';
 import logo from '../logo/macn-logo.png';
 
+const families = [
+    {
+      name: 'Family 1',
+      info: 'Some information',
+      kids: [
+        { name: 'Kid 1', class: 'Class 1', evaluation: 'Good' },
+      ],
+    },
+    {
+      name: 'Family 2',
+      info: 'Some information',
+      kids: [
+        { name: 'Kid 1', class: 'Class 1', evaluation: 'Good' },
+        { name: 'Kid 2', class: 'Class 2', evaluation: 'Good' },
+      ],
+    },
+    {
+      name: 'Family 3',
+      info: 'Some information',
+      kids: [
+        { name: 'Kid 1', class: 'Class 1', evaluation: 'Good' },
+        { name: 'Kid 2', class: 'Class 2', evaluation: 'Good' },
+        { name: 'Kid 3', class: 'Class 3', evaluation: 'Good' },
+      ],
+    },
+  ];
 
 function AdminFamily() {
   return (
@@ -41,65 +67,26 @@ function AdminFamily() {
                           
                     </button></div>
                 </div>
-                <div className='flex-container'>
-                    <div className='family-info'>
-                        <p>Name: Family 1</p>
-                        <p>Information: Some information</p>
-                        <p>Number of kids: 1</p>
-                    </div>
-                    <div className='vertical-line'></div>
-                    <div className='kids-info'>
-                        <div className='kid'>
-                            <p>Kid's Name: Kid 1</p>
-                            <p>Class: Class 1</p>
-                            <p>Evaluation: Good</p>
+                <div>
+                    {families.map((family, index) => (
+                    <div className='flex-container' key={index}>
+                        <div className='family-info'>
+                            <p>Name: {family.name}</p>
+                            <p>Information: {family.info}</p>
+                            <p>Number of kids: {family.kids.length}</p>
+                        </div>
+                        <div className='vertical-line'></div>
+                        <div className='kids-info'>
+                            {family.kids.map((kid, kidIndex) => (
+                                <div className='kid' key={kidIndex}>
+                                    <p>Kid's Name: {kid.name}</p>
+                                    <p>Class: {kid.class}</p>
+                                    <p>Evaluation: {kid.evaluation}</p>
+                                </div>
+                            ))}
                         </div>
                     </div>
-                </div>
-                <div className='flex-container'>
-                    <div className='family-info'>
-                        <p>Name: Family 2</p>
-                        <p>Information: Some information</p>
-                        <p>Number of kids: 2</p>
-                    </div>
-                    <div className='vertical-line'></div>
-                    <div className='kids-info'>
-                        <div className='kid'>
-                            <p>Kid's Name: Kid 1</p>
-                            <p>Class: Class 1</p>
-                            <p>Evaluation: Good</p>
-                        </div>
-                        <div className='kid'>
-                            <p>Kid's Name: Kid 2</p>
-                            <p>Class: Class 2</p>
-                            <p>Evaluation: Good</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='flex-container'>
-                    <div className='family-info'>
-                        <p>Name: Family 3</p>
-                        <p>Information: Some information</p>
-                        <p>Number of kids: 3</p>
-                    </div>
-                    <div className='vertical-line'></div>
-                    <div className='kids-info'>
-                        <div className='kid'>
-                            <p>Kid's Name: Kid 1</p>
-                            <p>Class: Class 1</p>
-                            <p>Evaluation: Good</p>
-                        </div>
-                        <div className='kid'>
-                            <p>Kid's Name: Kid 2</p>
-                            <p>Class: Class 2</p>
-                            <p>Evaluation: Good</p>
-                        </div>
-                        <div className='kid'>
-                            <p>Kid's Name: Kid 3</p>
-                            <p>Class: Class 3</p>
-                            <p>Evaluation: Good</p>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </div>        
