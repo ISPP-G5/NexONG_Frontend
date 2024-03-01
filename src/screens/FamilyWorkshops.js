83e68f4fc4a5191f99d46d00ecd633248e025f01
FranciscoRossoR <franrosora@gmail.com>
2024-03-01 18:37:18 +0100

    fix: family workshops screen style

--- src/screens/FamilyWorkshops.js
+++ src/screens/FamilyWorkshops.js
@@ -1,28 +1,28 @@
 import '../styles/styles.css';
+import familyWorkshops from '../logo/family-workshops.jpg'
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function FamilyWorkshops() {
     return (
         <div className="App">
-
             <Header/>
-
             <div className='main'>
-                <div className='title-text'>TALLERES FAMILIARES</div>
-                <div className='flex-container-blue'>
-                    Actividades formativas y de apoyo para las familias
+                <img src={familyWorkshops} alt="FamilyWorkshops" className='background-pic'/>
+                <div className='bg-text'>
+                    <h1>TALLERES FAMILIARES</h1>
+                    <h3>
+                        Actividades formativas y de apoyo para las familias
+                    </h3>
                 </div>
-                <div className='text'>
-                    Sesiones quincenales
+                <div className='flex-container'>
+                    <h3>
+                        Sesiones quincenales
+                    </h3>
                 </div>
             </div>
-
             <Footer/>
-
         </div>
-        
-
     );
 }
 export default FamilyWorkshops;
