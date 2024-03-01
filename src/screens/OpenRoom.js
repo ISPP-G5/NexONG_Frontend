f9538dd2b25fb45cd439987cb8d562aba32eb14e
FranciscoRossoR <franrosora@gmail.com>
2024-03-01 22:39:46 +0100

    fix: Open Room screen info

--- src/screens/OpenRoom.js
+++ src/screens/OpenRoom.js
@@ -18,9 +18,20 @@ function OpenRoom() {
                 </div>
                 <div className='flex-container'>
                     <h3>
-                        Lunes a jueves
+                        El programa escolar está destinado a mejorar las
+                        perspectivas escolares de los chicos y chicas.
                         <br/>
-                        16:00 a 20:00
+                        Trabajamos a través del refuerzo de destrezas básicas,
+                        de la mejora en el hábito lector y de la incorporación
+                        plena al ritmo de trabajo en el aula en la exigencia de
+                        las diferentes materias.
+                        <br/>
+                        Aportamos a cada alumno/a nuevas herramientas en su
+                        proceso de aprendizaje de tal forma que pueda aplicarlas
+                        a otras áreas académicas y a su propio desarrollo
+                        evolutivo y le sean de utilidad en posteriores aprendizajes.
+                        En la intervención además se trabajan las dificultades
+                        específicas que se presenten en las diferentes asignaturas.
                     </h3>
                 </div>
             </div>
