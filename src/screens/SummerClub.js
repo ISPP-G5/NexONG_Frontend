eb037df3464ec41e152ea9ff8ebbd0c830433d03
FranciscoRossoR <franrosora@gmail.com>
2024-03-01 19:06:38 +0100

    fix: summer club screen style

--- src/screens/SummerClub.js
+++ src/screens/SummerClub.js
@@ -1,29 +1,29 @@
 import '../styles/styles.css';
+import summerClub from '../logo/summer-club.jpg'
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function SummerClub() {
     return (
         <div className="App">
-
             <Header/>
-
             <div className='main'>
-                <div className='title-text'>CLUB DE VERANO</div>
-                <div className='flex-container-blue'>
-                    Club de verano para alumnos de primaria 
+                <img src={summerClub} alt="SummerClub" className='background-pic'/>
+                <div className='bg-text'>
+                    <h1>CLUB DE VERANO</h1>
+                    <h3>
+                        Club de verano para alumnos de primaria
+                    </h3>
                 </div>
-                <div className='text'>
-                    Ven y disfruta de actividades como manualidades,
-                    educación en valores y mucho más
+                <div className='flex-container'>
+                    <h3>
+                        Ven y disfruta de actividades como manualidades,
+                        educación en valores y mucho más
+                    </h3>
                 </div>
             </div>
-
             <Footer/>
-
         </div>
-        
-
     );
 }
 export default SummerClub;
