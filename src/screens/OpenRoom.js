20ee56156c449a9a4146f00f1f45fa3ad05c7486
FranciscoRossoR <franrosora@gmail.com>
2024-03-01 18:22:23 +0100

    fix: open room screen style

--- src/screens/OpenRoom.js
+++ src/screens/OpenRoom.js
@@ -1,31 +1,31 @@
 import '../styles/styles.css';
+import openRoom from '../logo/open-room.jpg'
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function OpenRoom() {
     return (
         <div className="App">
-
             <Header/>
-
             <div className='main'>
-                <div className='title-text'>AULA ABIERTA</div>
-                <div className='flex-container-blue'>
-                    Refuerzo educativo para menores entre 6 y 18 años así
-                    como actividades de relajacion y reflexión
+                <img src={openRoom} alt="OpenRoom" className='background-pic'/>
+                <div className='bg-text'>
+                    <h1>AULA ABIERTA</h1>
+                    <h3>
+                        Refuerzo educativo para menores entre 6 y 18 años así
+                        como actividades de relajacion y reflexión
+                    </h3>
                 </div>
-                <div className='text'>
-                    Lunes a jueves
-                    <br/>
-                    16:00 a 20:00
+                <div className='flex-container'>
+                    <h3>
+                        Lunes a jueves
+                        <br/>
+                        16:00 a 20:00
+                    </h3>
                 </div>
             </div>
-
             <Footer/>
-
         </div>
-        
-
     );
 }
 export default OpenRoom;
