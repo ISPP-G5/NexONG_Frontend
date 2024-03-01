6bececc05df0a78c8d4f851e1719997481a0dacb
MartaInes <73229109+MartaInes@users.noreply.github.com>
2024-03-01 17:44:59 +0100

    fix: codacy duplication

--- src/screens/Transparency.js
+++ src/screens/Transparency.js
@@ -34,45 +34,45 @@ Por ello, si quieres saber más sobre nosotros, podrás encontrar documentación
                     <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Código-etico-conducta.pdf">Código ético y de conducta</a></td>
                 </tr>
                 <tr>
-                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Estatutos-Asoxiación.pdf">Acta funcional</a></td>
-                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Estatutos-Asoxiación.pdf">Memoria anual 2015</a></td>
-                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Estatutos-Asoxiación.pdf">Memoria económica 2015</a></td>
-                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Estatutos-Asoxiación.pdf">Balance cuentas 2015</a></td>
-                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Estatutos-Asoxiación.pdf">Código ético de voluntariado</a></td>
+                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" >Acta funcional</a></td>
+                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" >Memoria anual 2015</a></td>
+                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" >Memoria económica 2015</a></td>
+                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" >Balance cuentas 2015</a></td>
+                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" >Código ético de voluntariado</a></td>
                 </tr>
                 <tr>
-                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Estatutos-Asoxiación.pdf">Inscripción Registro de Asociaciones</a></td>
-                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Estatutos-Asoxiación.pdf">Memoria anual 2016</a></td>
-                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Estatutos-Asoxiación.pdf">Memoria económica 2016</a></td>
-                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Estatutos-Asoxiación.pdf">Balance cuentas 2016</a></td>
-                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Estatutos-Asoxiación.pdf">Calidad, buen gobierno y buenas prácticas</a></td>
+                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" >Inscripción Registro de Asociaciones</a></td>
+                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" >Memoria anual 2016</a></td>
+                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" >Memoria económica 2016</a></td>
+                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" >Balance cuentas 2016</a></td>
+                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" >Calidad, buen gobierno y buenas prácticas</a></td>
                 </tr>
                 <tr>
-                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Estatutos-Asoxiación.pdf">Registro General de Entidades de Voluntariado</a></td>
-                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Estatutos-Asoxiación.pdf">Memoria anual 2017</a></td>
-                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Estatutos-Asoxiación.pdf">Memoria económica 2017</a></td>
-                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Estatutos-Asoxiación.pdf">Balance cuentas 2017</a></td>
-                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Estatutos-Asoxiación.pdf">Memoria igualdad de género</a></td>
-                </tr>
-                <tr>
-                <td></td>
-                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Estatutos-Asoxiación.pdf">Memoria anual 2018</a></td>
-                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Estatutos-Asoxiación.pdf">Memoria económica 2018</a></td>
-                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Estatutos-Asoxiación.pdf">Balance cuentas 2018</a></td>
-                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Estatutos-Asoxiación.pdf">Dossier de prensa 2022</a></td>
+                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" >Registro General de Entidades de Voluntariado</a></td>
+                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" >Memoria anual 2017</a></td>
+                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" >Memoria económica 2017</a></td>
+                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" >Balance cuentas 2017</a></td>
+                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" >Memoria igualdad de género</a></td>
                 </tr>
                 <tr>
                 <td></td>
-                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Estatutos-Asoxiación.pdf">Memoria anual 2019</a></td>
-                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Estatutos-Asoxiación.pdf">Memoria económica 2019</a></td>
-                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Estatutos-Asoxiación.pdf">Balance cuentas 2019</a></td>
+                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" >Memoria anual 2018</a></td>
+                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" >Memoria económica 2018</a></td>
+                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" >Balance cuentas 2018</a></td>
+                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" >Dossier de prensa 2022</a></td>
+                </tr>
+                <tr>
+                <td></td>
+                <td><a href='' target="_blank">Memoria anual 2019</a></td>
+                <td><a href='' target="_blank">Memoria económica 2019</a></td>
+                <td><a href='' target="_blank">Balance cuentas 2019</a></td>
                 <td></td>
                 </tr>
                 <tr>
                 <td></td>
-                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Estatutos-Asoxiación.pdf">Memoria anual 2020</a></td>
-                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Estatutos-Asoxiación.pdf">Memoria económica 2020</a></td>
-                <td><a href='http://manosabiertasnorte.es/files/Estatutos-2019--2-.pdf' target="_blank" download="Estatutos-Asoxiación.pdf">Balance cuentas 2020</a></td>
+                <td><a href='' target="_blank">Memoria anual 2020</a></td>
+                <td><a href='' target="_blank">Memoria económica 2020</a></td>
+                <td><a href='' target="_blank">Balance cuentas 2020</a></td>
                 <td></td>
                 </tr>
                 <tr>
