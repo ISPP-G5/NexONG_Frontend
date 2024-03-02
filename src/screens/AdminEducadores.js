248712aca9cf4bb17cea1faaca7344ff6eb32e77
claugp06 <99924348+claugp06@users.noreply.github.com>
2024-03-02 20:53:22 +0100

    Feat:Educadores

--- src/screens/AdminEducadores.js
+++ src/screens/AdminEducadores.js
@@ -4,43 +4,43 @@ import HeaderAdmin from '../components/HeaderAdmin';
 import MenuAdmin from '../components/MenuAdmin';
 
 const educadores = [
-    { name: 'Educador 1', class: 'Class 1' },
-    { name: 'Educador 2', class: 'Class 2' },
-    { name: 'Educador 3', class: 'Class 3' },
+  { name: 'Educador 1', className: 'Class 1' },
+  { name: 'Educador 2', className: 'Class 2' },
+  { name: 'Educador 3', className: 'Class 3' },
 ];
 
-function EducadorCard({ name, class }) {
-    return (
-        <div className='card-info'>
-            <div className='family-request'>
-                <img src='https://via.placeholder.com/150' alt='placeholder' />
-                <div className='family-info'>
-                    <p>{name}</p>
-                    <p>{class}</p>
-                </div>
-            </div>
-        </div>
-    );
+function EducadorCard({ name, className }) {
+  return (
+      <div className='card-info'>
+          <div className='family-request'>
+              <img src='https://via.placeholder.com/150' alt='placeholder' />
+              <div className='family-info'>
+                  <p>{name}</p>
+                  <p>{className}</p>
+              </div>
+          </div>
+      </div>
+  );
 }
 
 function AdminEducador() {
-    return (
-        <div className='App'>
-            <HeaderAdmin />
-            <div className='admin-main'>
-                <MenuAdmin selected='Educadores' />
-                <div className='vertical-line'></div>
-                <div className='admin-container'>
-                    <div className='pantallas'>
-                        <a href='/AdminEducadores'>Nuestros educadores</a>
-                    </div>
-                    {educadores.map((educador) => (
-                        <EducadorCard name={educador.name} class={educador.class} />
-                    ))}
-                </div>
-            </div>
-        </div>
-    );
+  return (
+      <div className='App'>
+          <HeaderAdmin />
+          <div className='admin-main'>
+              <MenuAdmin selected='Educadores' />
+              <div className='vertical-line'></div>
+              <div className='admin-container'>
+                  <div className='pantallas'>
+                      <a href='/AdminEducadores'>Nuestros educadores</a>
+                  </div>
+                  {educadores.map((educador) => (
+                      <EducadorCard name={educador.name} className={educador.className} />
+                  ))}
+              </div>
+          </div>
+      </div>
+  );
 }
 
 export default AdminEducador;
