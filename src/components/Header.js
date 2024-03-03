f327e7009891f7e2fa68d595a2af307e8ea42064
claugp06 <99924348+claugp06@users.noreply.github.com>
2024-03-03 21:19:45 +0100

    Fix: duplicated elements in the dropdown

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,11 +36,6 @@ function Header(){
                 <div className="dropdown-content">
                     <Link to="/nosotros">Nosotros</Link>
                     <Link to="/historia">Historia</Link>
-                    <Link to="/mision-vision-valores">Misión, Visión y Valores</Link>
-                    <Link to="/">Nosotros</Link>
-                    <Link to="/">Historia</Link>
-                    <Link to="/">Misión, Visión y Valores</Link>
-                    <Link to="/">La Salle</Link>
                     <Link to="/donde-estamos">Dónde estamos</Link>
                     <Link to="/">Organización</Link>
                     <Link to="/transparencia">Transparencia</Link>
