be2df6a4f0ddaebae386058ce3f003844451df6c
manbarjim2 <80253313+manbarjim2@users.noreply.github.com>
2024-03-03 16:53:10 +0100

    fix: mision, overview and values in flex-containers

--- src/screens/MisionOverviewValues.js
+++ src/screens/MisionOverviewValues.js
@@ -9,19 +9,27 @@ function MisionOverviewValues() {
         <div className="App">
             <Header/>
             <div className='main'>
-            <img src={people} alt="People" className='background-pic'/> 
+                <img src={people} alt="People" className='background-pic'/> 
                 <div className='bg-text'>
-                    <h1>MISIÓN</h1>
-                    <h3>
-                        La identidad se forja mediante educación no formal adaptable a necesidades locales descubiertas mediante estudios y observación directa.</h3>
-                    <h1>VISIÓN</h1>
-                    <h3>Buscamos ser referentes en intervención socioeducativa en el norte de Sevilla, ofreciendo recursos y apoyo comunitario para reducir la desigualdad, destacando por nuestra cercanía, adaptabilidad y programas holísticos.</h3>
-                    <h1>VALORES</h1>
-                    <h3>Respeto, Resposabilidad, Compromiso y Solidaridad</h3>
-                    <h3>Libertad e Igualdad, Democracia y participación, Accesibilidad</h3>
-                    <h3>Cooperación y colaboración, Creatividad e Innovación</h3>
+                    <h1>MISIÓN, VISIÓN Y VALORES</h1>
                 </div>   
             </div>
+            <div className='homepage-container'>
+                    <div className='flex-container'>
+                        <h1>MISIÓN</h1>
+                        <h3> La identidad se forja mediante educación no formal adaptable a necesidades locales descubiertas mediante estudios y observación directa.</h3>
+                    </div>
+                    <div className='flex-container'>
+                        <h1>VISIÓN</h1>
+                        <h3>Buscamos ser referentes en intervención socioeducativa en el norte de Sevilla, ofreciendo recursos y apoyo comunitario para reducir la desigualdad, destacando por nuestra cercanía, adaptabilidad y programas holísticos.</h3>
+                    </div>
+                    <div className='flex-container'>
+                        <h1>VALORES</h1>
+                        <h3>Respeto, Resposabilidad, Compromiso y Solidaridad</h3>
+                        <h3>Libertad e Igualdad, Democracia y participación, Accesibilidad</h3>
+                        <h3>Cooperación y colaboración, Creatividad e Innovación</h3>
+                    </div>
+                </div>
             <Footer/>
         </div>
     );
