9e1b3f235372d32283940773be177a5aa9103527
fracalrod3 <fracalrod3@alum.us.es>
2024-03-13 15:23:17 +0100

    feat: Back-End PUT

--- src/screens/AdminProfileUpdate.js
+++ src/screens/AdminProfileUpdate.js
@@ -1,9 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/styles.css';
 import AdminLayout from '../components/AdminLayout';
+import axios from 'axios';
+import { Email } from '@material-ui/icons';
+
+const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT;
 
 
 const AdminProfilesUpdate = () => {
+
+    const [valoresList, setValores] = useState([]);
+
+    useEffect(() => {
+
+      axios.get(`${API_ENDPOINT}user/`)
+        .then(response => {
+          setValores(response.data.filter(x=>x.id==parseInt(localStorage.getItem('userId'),10)));
+        })
+        .catch(error => {
+          console.error(error);
+        });
+
+    }, []);
+
+    const [id, setId] = useState(parseInt(localStorage.getItem('userId'),10));
+    const [name, setName] = useState("");
+    const [surname, setSurname] = useState("");
+    const [id_number, setId_number] = useState("");
+    const [phone, setPhone] = useState(null);
+    const [password, setPassword] = useState("");
+
+    const [pass2, setPass2] = useState("");
+
+    const [email, setEmail] = useState("");
+    const [role, setRole] = useState("");
+    const [avatar, setAvatar] = useState("");
+
+   const updateAdmin = async () => {
+    if(pass2!=password){
+        window.alert("¡Las contraseñas no coinciden!");
+    }else if(!(email.includes("@") || email.includes("."))){
+        window.alert("¡Debes introducir un correo electrónico!");
+    }else if(!name || name===""){
+        window.alert("¡Es necesario un nombre!");
+    }else if(!phone || phone==null || phone.length()!=9){
+        window.alert("¡Es necesario un número de teléfono!");
+    }else{
+        const update = await axios.put(`${API_ENDPOINT}user/${id}`,{
+            name:name,
+            surname:valoresList[0].surname,
+            id_number:valoresList[0].id_number,
+            phone:phone,
+            password:password,
+            email:email,
+            role:valoresList[0].role,
+            avatar:"https://static.vecteezy.com/system/resources/previews/015/665/684/non_2x/man-with-the-inscription-admin-icon-outline-style-vector.jpg",
+        });
+        console.log('update',update);
+        const {data} = update;
+        if (data.message){
+            window.alert(data.message);
+        }else{
+            window.alert("Usuario actualizado con éxito.")
+        }
+    }
+   }
+  
+
     return (
       <div className='App'>
          
@@ -16,22 +79,38 @@ const AdminProfilesUpdate = () => {
                       borderRadius: '100%',
                     }}  />
                 </div>
-                    <div style={{ alignSelf: 'center', fontWeight: 'bold'}}
-                        >Nombre</div>
-                        
+
+                    <div className='bold-text'>Nombre</div>
+                        <input value={name} 
+                        onChange={(e) => setName(e.target.value)} 
+                        type='text' 
+                        placeholder='Nombre'></input>
+
                     <div className='bold-text'>Email</div>
-                        <input type='text' placeholder='Email'></input>
+                        <input value={email} 
+                        onChange={(e) => setEmail(e.target.value)} 
+                        type='text' 
+                        placeholder='Email'></input>
 
                     <div className='bold-text'>Teléfono</div>
-                        <input type='text' placeholder='Número de teléfono'></input>
+                        <input value={phone}
+                        onChange={(e) => setPhone(e.target.value)} 
+                        type='text' 
+                        placeholder='Número de teléfono'></input>
 
                     <div className='bold-text'>Contraseña</div>
-                        <input type='text' placeholder='Contraseña'></input>
+                        <input value={password}
+                        onChange={(e) => setPassword(e.target.value)} 
+                        type='text' 
+                        placeholder='Contraseña'></input>
                     
                     <div className='bold-text'>Repetir contraseña</div>
-                        <input type='text' placeholder='Contraseña'></input>
+                        <input value={pass2}
+                        onChange={(e) => setPass2(e.target.value)}
+                        type='text' 
+                        placeholder='Contraseña'></input>
 
-                    <button className='button' style={{textAlign:'center', alignSelf:'center', margin:'4%'}}>
+                    <button onClick={updateAdmin} className='button' style={{textAlign:'center', alignSelf:'center', margin:'4%'}}>
                             Actualizar perfil
                     </button>
               </div>
