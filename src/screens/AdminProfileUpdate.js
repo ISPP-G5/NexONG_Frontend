8f089a410616ceb14c057dbd8e0831e33be0574f
fracalrod3 <fracalrod3@alum.us.es>
2024-03-13 17:17:35 +0100

    fix: Everything solved and connected

--- src/screens/AdminProfileUpdate.js
+++ src/screens/AdminProfileUpdate.js
@@ -1,33 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import '../styles/styles.css';
 import AdminLayout from '../components/AdminLayout';
 import axios from 'axios';
-import { Email } from '@material-ui/icons';
 
 const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT;
 
 
 const AdminProfilesUpdate = () => {
 
-    const [valoresList, setValores] = useState([]);
-
-    useEffect(() => {
-
-      axios.get(`${API_ENDPOINT}user/`)
-        .then(response => {
-          setValores(response.data.filter(x=>x.id==parseInt(localStorage.getItem('userId'),10)));
-        })
-        .catch(error => {
-          console.error(error);
-        });
-
-    }, []);
 
     const [id, setId] = useState(parseInt(localStorage.getItem('userId'),10));
     const [name, setName] = useState("");
     const [surname, setSurname] = useState("");
     const [id_number, setId_number] = useState("");
-    const [phone, setPhone] = useState(null);
+    const [phone, setPhone] = useState("");
     const [password, setPassword] = useState("");
 
     const [pass2, setPass2] = useState("");
@@ -35,18 +21,18 @@ const AdminProfilesUpdate = () => {
     const [email, setEmail] = useState("");
 
    const updateAdmin = async () => {
-    if(pass2!=password){
+    if(pass2!==password){
         window.alert("¡Las contraseñas no coinciden!");
     }else{
-        const update = await axios.put(`${API_ENDPOINT}user/${id}`,{
-            name:name,
-            surname:surname,
-            id_number:id_number,
-            phone:phone,
-            password:password,
-            email:email,
-            role:"ADMIN",
-            avatar:"https://static.vecteezy.com/system/resources/previews/015/665/684/non_2x/man-with-the-inscription-admin-icon-outline-style-vector.jpg",
+        const update = await axios.put(`${API_ENDPOINT}user/${id}/`,{
+            name: name,
+            surname: surname,
+            id_number: id_number,
+            phone : phone,
+            role: "ADMIN",
+            password: password,
+            email: email,
+            avatar: "https://static.vecteezy.com/system/resources/previews/015/665/684/non_2x/man-with-the-inscription-admin-icon-outline-style-vector.jpg",
         });
         console.log('update',update);
         const {data} = update;
@@ -72,7 +58,6 @@ const AdminProfilesUpdate = () => {
                     }}  />
                 </div>
                 
-                <form>
                     <div className='bold-text'>Nombre</div>
                         <input value={name} 
                         onChange={(e) => setName(e.target.value)} 
@@ -85,11 +70,11 @@ const AdminProfilesUpdate = () => {
                         type='text' 
                         placeholder='Primer Apellido'></input>
 
-                    <div className='bold-text'>Número identificativo</div>
+                    <div className='bold-text'>Identificación</div>
                         <input value={id_number} 
                         onChange={(e) => setId_number(e.target.value)} 
-                        type='number' 
-                        placeholder='Número identificativo'></input>
+                        type='text' 
+                        placeholder='DNI/NIE/Pasaporte'></input>
 
                     <div className='bold-text'>Email</div>
                         <input value={email} 
@@ -118,7 +103,6 @@ const AdminProfilesUpdate = () => {
                     <button onClick={updateAdmin} className='button' style={{textAlign:'center', alignSelf:'center', margin:'4%'}}>
                             Actualizar perfil
                     </button>
-                </form>
               </div>
               </AdminLayout>
         </div>
