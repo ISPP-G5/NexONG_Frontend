413bad19db9a650ecf433a518aec48ca3e4b09f4
Iván Ramírez <ivaramlar@alum.us.es>
2024-03-22 15:26:49 +0100

    fix: Implemented to accept a volunteer

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,5 +1,7 @@
 import DeleteIcon from '@material-ui/icons/Delete';
 import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT;
 
 function PersonCard({ person, personType, kids, request = false, trash = true }) {
@@ -7,7 +9,7 @@ function PersonCard({ person, personType, kids, request = false, trash = true })
   const handleDescargar = async(person) => {
     // Download the enrollment_document
     const documento = person.enrollment_document;
-    const nombreArchivo = 'documento.pdf'; // Puedes cambiar el nombre del archivo según lo necesites
+    const nombreArchivo = 'documento_de_enlistamiento.pdf'; // Puedes cambiar el nombre del archivo según lo necesites
 
     // Crear un objeto Blob a partir de los datos del documento
     const blob = new Blob([documento], { type: 'application/pdf' });
@@ -19,41 +21,37 @@ function PersonCard({ person, personType, kids, request = false, trash = true })
 
     // Hacer clic en el enlace para descargar el archivo
     enlaceDescarga.click();
-    window.alert("Se descarga vacio porque la api pasa enlaces en vez de archivos");
+    toast.error("Se descarga vacio porque la api pasa enlaces en vez de archivos", {
+      autoClose: 5000
+      });
   };
   
   const handleAceptar = async (person) => {
-    /*const update = await axios.put(`${API_ENDPOINT}volunteer/${voluntario[0].id}/`,{
-        academic_formation: voluntario[0].academic_formation,
-        motivation: voluntario[0].motivation,
-        status: voluntario[0].status,
-        address:voluntario[0].address,
-        postal_code: voluntario[0].postal_code,
-        enrollment_document: voluntario[0].enrollment_document,
-        registry_sheet: voluntario[0].registry_sheet,
-        sexual_offenses_document: voluntario[0].sexual_offenses_document,
-        scanned_id: voluntario[0].scanned_id,
-        minor_authorization: voluntario[0].minor_authorization,
-        scanned_authorizer_id: voluntario[0].scanned_authorizer_id,
-        birthdate: voluntario[0].birthdate,
-        start_date: voluntario[0].start_date,
-        end_date: null,
-
-        
+    console.log(person)
+    person.status = "ACEPTADO";
+    
+    const update = await axios.patch(`${API_ENDPOINT}volunteer/${person.volunteer}/`,{
+        status: person.status        
     });
     console.log('update',update);
     const {data} = update;
     if (data.message){
-        window.alert(data.message);
+        toast.error("Error al actualizar", {
+          autoClose: 5000
+          });
     }else{
-        window.alert("Usuario actualizado con éxito.")
-    }*/
-    window.alert("Usuario no puede ser aceptado, la funcionalidad esta comentada hasta nuevo aviso.")
+        toast.success("Usuario actualizado con éxito.", {
+          autoClose: 5000
+          })
+    }
+    window.location.reload();
   }
   
   const handleRechazarOEliminar = async(person) =>{
     if(!person.id || person.id <= 0){
-        window.alert('La id no es valida')
+        toast.error('La id no es valida', {
+          autoClose: 5000
+          })
     }else{
       if(personType === 'Familias-solicitudes'){
         await axios.delete(`${API_ENDPOINT}student/${person.id}/`);
@@ -61,13 +59,16 @@ function PersonCard({ person, personType, kids, request = false, trash = true })
       else {
         await axios.delete(`${API_ENDPOINT}user/${person.id}/`);
       }
-        window.alert("Persona eliminada correctamente")
+        toast.success("Persona eliminada correctamente", {
+          autoClose: 5000
+          })
         window.location.reload(); // Recarga la ventana después de eliminar
     }
   }
 
   return (
     <div className='card-info'>
+      <ToastContainer autoClose={5000} />
       {personType === 'Familias' ?
         <div className='family-info'>
           <p>{person.first_name}</p>
