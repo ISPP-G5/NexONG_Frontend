66ab8a7ad5f2867b5fc5e60a8122b0d9793ec2b8
Iván Ramírez <ivaramlar@alum.us.es>
2024-03-22 16:46:12 +0100

    fix:Solved the bug from parnter, now the botton and partners are visible

--- src/screens/admin/AdminPartners.js
+++ src/screens/admin/AdminPartners.js
@@ -1,8 +1,10 @@
-import React,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../styles/styles.css';
 import ShowType from '../../components/ShowAdminProfiles';
 import axios from 'axios';
 import DownloadIcon from '@mui/icons-material/Download';
+import { useFetchUsersByRole } from '../../components/useFetchData';
+
 const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT
 
 const pantallas = [
@@ -17,35 +19,41 @@ const pantallas = [
     selected: false,
   }
 ];
- 
+
 
 const AdminPartners = () => {
-  const [donations,setDonations] = useState ([]);
+  const [donations, setDonations] = useState([]);
   useEffect(() => {
-      axios.get(`${API_ENDPOINT}donation/`)
-        .then(response => {
-            setDonations(response.data)
-          }
-        );
-    
+    axios.get(`${API_ENDPOINT}donation/`)
+      .then(response => {
+        setDonations(response.data)
+      }
+      );
+
   }, []);
-  
+
   const handleDownload = () => {
     window.location.href = 'http://localhost:8000/api/export/pdf/donations';
   }
+  const userPartners = useFetchUsersByRole(API_ENDPOINT, "SOCIO");
+
   return (
     <div>
-      <ShowType type = "PARTNER" pantallas={pantallas}></ShowType>
-      <div style={{ display: 'flex', justifyContent: 'flex-end',marginRight: '5%' }}>
-      <button className="button-create" onClick={handleDownload}>
-        <DownloadIcon />
-        Donaciones
-      </button>
+      <ShowType
+        data={userPartners}
+        type="Socios"
+        pantallas={pantallas}
+      />
+      <div style={{ display: 'flex', justifyContent: 'flex-end', marginRight: '5%' }}>
+        <button className="button-create" onClick={handleDownload}>
+          <DownloadIcon />
+          Donaciones
+        </button>
 
       </div>
     </div>
   );
-   
+
 }
 
 export default AdminPartners;
