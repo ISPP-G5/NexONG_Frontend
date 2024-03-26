1b67098847eb0b73baa22ed473cad9895d7fecfa
Sammy <samrodman@alum.us.es>
2024-03-26 15:15:44 +0100

    fix: button Conócenos homepage

--- src/screens/homepage/HomePage.js
+++ src/screens/homepage/HomePage.js
@@ -9,6 +9,8 @@ const info = [
     {
       title: 'Nosotros',
       description: 'Asociación Manos Abiertas con Norte es una entidad sin ánimo de lucro, perteneciente a la Red de Obras Socioeducativas de La Salle, que trabaja en el ámbito de la intervención social en la barriada Polígono Norte y en el asentamiento chabolista de El Vacie, Sevilla.',
+      link: '/asociacion/nosotros',
+      button: 'Conócenos',
     },
     {
       title: 'Entidades Colaboradoras',
@@ -40,7 +42,7 @@ function HomePage() {
           <LayoutHomepage 
             title={'Asociación Manos Abiertas con Norte'} 
             description={'Manos Abiertas surge como iniciativa en 1992. Un grupo de jóvenes voluntarios/as, detecta necesidades socioeducativas en la zona de Polígono Norte, Sevilla, y comienza a impartir clases de apoyo de matemáticas y lengua a los niños y niñas de los centros educativos de la zona: Blas Infante y Josefa Amor y Rico (Actualmente IES Inmaculada Vieira), en locales situados en bloques de la barriada.'}
-            image={'ong'}
+            image={'ong'} 
             info={info}
           />        
          );
