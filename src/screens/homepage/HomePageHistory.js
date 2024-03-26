7d1d3430051217c1be9ee003006fd715b7445bc7
Sammy <samrodman@alum.us.es>
2024-03-26 14:53:17 +0100

    fix: text of history page appears

--- src/screens/homepage/HomePageHistory.js
+++ src/screens/homepage/HomePageHistory.js
@@ -3,19 +3,23 @@ import '../../styles/styles.css';
 import { useEffect } from 'react';
 import chronology from '../../logo/actividades-desarrolladas.jpg';
 import LayoutHomepage from '../../components/LayoutHomepage';
-
+import HistoryDescription from '../../components/HistoryDescription';
 function HomePageHistory() {
     useEffect(() => {
         window.scrollTo(0, 0);
       }, []);
       
+
+    
+      
     return (
         <LayoutHomepage 
             title="HISTORIA" 
-            description="En 1992, jóvenes voluntarios en Sevilla fundaron Manos Abiertas para ofrecer clases de apoyo a niños del Polígono Norte. En 2015, evolucionaron a Asociación Manos Abiertas con Norte, profesionalizando su intervención y colaborando con diversas entidades. Hoy, cuentan con un equipo técnico, voluntarios y colaboradores, manteniendo su compromiso con la comunidad."
+            description={<HistoryDescription />} // Use the DescriptionComponent here
+            
             image={'activities'}
         > 
-
+            <p>Las actividades han variado a lo largo de estos años según se muestra en el siguiente cuadro:</p>
             <img src={chronology} alt="Cronología de la Asociación Manos Abiertas con Norte" />
             
         </LayoutHomepage>
