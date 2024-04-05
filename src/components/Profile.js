7ac292af7a952a9db9556dc46a32f706c523a3d6
fracalrod3 <fracalrod3@alum.us.es>
2024-04-05 19:00:21 +0200

    fix: Profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,37 +8,42 @@ const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT;
 const Profile = ({usuario}) => {
 
   const [valores, setValores] = useState([]);
+  const [name, setName] = useState("")
 
   //Traemos los datos del usuario que ha iniciado sesión
   useEffect(() => {
 
-      axios.get(`${API_ENDPOINT}user/`)
+      axios.get(`${API_ENDPOINT}auth/users/me`)
         .then(response => {
-          setValores(response.data.filter(x=>x.id===parseInt(localStorage.getItem('userId'),10)));
+          setValores(response.data);
         })
         .catch(error => {
           console.error(error);
         });
 
+        setTimeout(setName(valores.first_name + " " + valores.last_name),900);
   }, []);
+
+
 
   //Mostramos los datos en inputs para censurar la contraseña
   return (
     <div  className='register-container admin' style={{width: '300px', marginTop:'6%'}}>
-      {valores.map((profile, index) => (
-        <div key={index}>
-          <img src={profile.avatar} alt={"imagen"} />
+          <img src={valores.avatar} alt={"imagen"} />
 
-          <div style={{ alignSelf: 'center', fontWeight: 'bold', marginTop: '1%', marginBottom:'1%' }}>{profile.username}</div>
+          <div style={{ alignSelf: 'center', fontWeight: 'bold', marginTop: '1%', marginBottom:'1%' }}>{valores.username}</div>
+
+          <p>Usuario</p>
+          <input type='text' value={name} readOnly></input>
 
           <p>Email</p>
-          <input type='text' value={profile.email} readOnly></input>
+          <input type='text' value={valores.email} readOnly></input>
 
           <p>Teléfono</p>
-          <input type='text' value={profile.phone} readOnly></input>
+          <input type='text' value={valores.phone} readOnly></input>
 
-          <p>Contraseña</p>
-          <input type='password' value={profile.password} readOnly></input>
+          <p>DNI/NIE/Pasaporte</p>
+          <input type='text' value={valores.id_number} readOnly></input>
 
           <button className='register-button admin' >
             <Link to={`/${usuario}/perfil/actualizar`}>
@@ -46,8 +51,6 @@ const Profile = ({usuario}) => {
             </Link>
           </button>
         </div>
-      ))}
-    </div>
 
   );
 };
