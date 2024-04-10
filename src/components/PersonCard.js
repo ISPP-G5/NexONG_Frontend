9c2e76029355b5c1b3a1eb9efce6d3075e5d7d2b
Iván Ramírez <ivaramlar@alum.us.es>
2024-04-10 15:10:53 +0200

    fix:Solved the problems with family request

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,12 +1,14 @@
 import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
 const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT;
 
 function PersonCard({ person, personType, kids, request = false, trash = true }) {
 
-  const handleDescargar = async(person) => {
+  const handleDescargar = async (person) => {
     // Download the enrollment_document
     const documento = person.enrollment_document;
     const nombreArchivo = 'documento_de_enlistamiento.pdf'; // Puedes cambiar el nombre del archivo según lo necesites
@@ -23,46 +25,79 @@ function PersonCard({ person, personType, kids, request = false, trash = true })
     enlaceDescarga.click();
     toast.error("Se descarga vacio porque la api pasa enlaces en vez de archivos", {
       autoClose: 5000
-      });
-  };
-  
-  const handleAceptar = async (person) => {
-    console.log(person)
-    person.status = "ACEPTADO";
-    
-    const update = await axios.patch(`${API_ENDPOINT}volunteer/${person.volunteer}/`,{
-        status: person.status        
     });
-    console.log('update',update);
-    const {data} = update;
-    if (data.message){
-        toast.error("Error al actualizar", {
-          autoClose: 5000
-          });
-    }else{
-        toast.success("Usuario actualizado con éxito.", {
-          autoClose: 5000
-          })
+  };
+
+  const handleAceptar = async (person) => {
+    person.status = "ACEPTADO";
+    let url;
+    if (person.volunteer) {
+      url = `${API_ENDPOINT}volunteer/${person.volunteer}/`;
+    } else if (person.id) {
+      url = `${API_ENDPOINT}student/${person.id}/`;
+    }
+
+    const update = await axios.patch(url, {
+      status: person.status
+    });
+    console.log('update', update);
+    const { data } = update;
+    if (data.message) {
+      toast.error("Error al actualizar", {
+        autoClose: 5000
+      });
+    } else {
+      toast.success("Usuario actualizado con éxito.", {
+        autoClose: 5000
+      })
+    }
+    window.location.reload();
+}
+
+  const handleRechazar = async (person) => {
+    let url;
+    person.status = "RECHAZADO";
+    if (person.volunteer) {
+      url = `${API_ENDPOINT}volunteer/${person.volunteer}/`;
+    } else if (person.id) {
+      url = `${API_ENDPOINT}student/${person.id}/`;
+    }
+
+    const update = await axios.patch(url, {
+      status: person.status
+    });
+    console.log('update', update);
+    const { data } = update;
+    if (data.message) {
+      toast.error("Error al actualizar", {
+        autoClose: 5000
+      });
+    } else {
+      toast.success("Usuario actualizado con éxito.", {
+        autoClose: 5000
+      })
     }
     window.location.reload();
   }
-  
-  const handleRechazarOEliminar = async(person) =>{
-    if(!person.id || person.id <= 0){
-        toast.error('La id no es valida', {
-          autoClose: 5000
-          })
-    }else{
-      if(personType === 'Familias-solicitudes'){
+
+  const handleEliminar = async (person) => {
+    if (!person.id || person.id <= 0) {
+      toast.error('La id no es valida', {
+        autoClose: 5000
+      })
+    } else {
+      if (personType === 'Familias-solicitudes') {
         await axios.delete(`${API_ENDPOINT}student/${person.id}/`);
-      }
-      else {
+      } else if (personType === 'Voluntarios') {
+        console.log(person.id);
+        await axios.delete(`${API_ENDPOINT}volunteer/${person.volunteer}/`);
+      } else {
         await axios.delete(`${API_ENDPOINT}user/${person.id}/`);
       }
-        toast.success("Persona eliminada correctamente", {
-          autoClose: 5000
-          })
-        window.location.reload(); // Recarga la ventana después de eliminar
+      toast.success("Persona eliminada correctamente", {
+        autoClose: 5000
+      })
+      window.location.reload(); // Recarga la ventana después de eliminar
     }
   }
 
@@ -74,11 +109,11 @@ function PersonCard({ person, personType, kids, request = false, trash = true })
           <p>{person.first_name}</p>
           <p><strong>{person.name}</strong></p>
           <p>Número de niños: {kids.filter(kid => kid.family === person.id).length}</p>
-        </div> 
+        </div>
         :
         <div className='family-request'>
           <img src={person.avatar} alt='placeholder' />
-          <div className='family-info' style={{ borderRight: 'none', borderBottom: 'none'}}>
+          <div className='family-info' style={{ borderRight: 'none', borderBottom: 'none' }}>
             {personType === 'Familias-solicitudes' ? <p><strong>{person.first_name}</strong></p> : <p>{person.first_name}</p>}
             <p>{person.last_name}</p>
           </div>
@@ -105,11 +140,19 @@ function PersonCard({ person, personType, kids, request = false, trash = true })
           <button className='button-contrast' onClick={() => handleDescargar(person)}>Descargar</button>
           <div className='buttons-acceptance'>
             <button className='button-accept' onClick={() => handleAceptar(person)}>Aceptar</button>
-            <button className='button-decline' onClick={() => handleRechazarOEliminar(person)}>Rechazar</button>
+            <button className='button-decline' onClick={() => handleRechazar(person)}>Rechazar</button>
           </div>
         </div>
       }
-      {trash && <DeleteIcon className='trash' onClick={() => handleRechazarOEliminar(person)} />}
+      {trash &&
+        <div className='buttons-acceptance'>
+          <DeleteIcon className='trash' style={{marginLeft:'87.5%'}} onClick={() => handleEliminar(person)} />
+          <EditIcon className='edit' style={{marginLeft:'87.5%'}} onClick={() => window.location.replace(`/admin/perfil/actualizar/${person.id}`) } />
+        </div>
+      }
+
+
+
     </div>
   );
 }
