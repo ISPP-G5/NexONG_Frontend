a98be11212b610d548a64e4211afb5b20b8f91a3
claugp06 <99924348+claugp06@users.noreply.github.com>
2024-04-10 16:20:33 +0200

    Fix: text Obra La Salle

--- src/screens/homepage/HomePageLaSalle.js
+++ src/screens/homepage/HomePageLaSalle.js
@@ -21,7 +21,7 @@ function HomePageLaSalle() {
             <div className='agenda-intro'>
                 <div>
                     <h1>La Salle</h1>
-                    <p>En 2016 Asociación Manos Abiertas con Norte se suma a la Red de Obras Socioeducativas de La Salle. LA SALLE es una Institución implantada en casi un centenar de países que se dedica a la Educación. </p>
+                    <p>En 2016 Asociación Manos Abiertas con Norte se suma a la Red de Obras Socioeducativas de La Salle, institución implantada en casi un centenar de países que se dedica a la Educación. </p>
                     <p>Los Centros La Salle reciben su nombre de San Juan Bautista de La Salle quien, a finales del siglo XVII se asoció con unos maestros para mantener escuelas dedicadas a los niños pobres. Con ellos fundó el Instituto de los Hermanos de las Escuelas Cristianas.</p>
                 </div>
                 <img src={lasalle} alt='Asociación Rutas'></img>
@@ -31,8 +31,8 @@ function HomePageLaSalle() {
                 
                 <div>
                     <h1>OBRAS LA SALLE </h1>
-                    <p>la Red de Obras La Salle incluye Centros educativos, Centros educativos preuniversitarios y de formación profesional, universidades y Obras socioeducativas en España y Portugal, éstas obras se encuentran agrupadas en la Coordinadora de Obras Socioeducativas La Salle.</p>
-                    <p>En la actualidad ocho entidades forman parte esta Coordinadora La Salle y otras tres colaboran con ella.</p>
+                    <p>La Red de Obras La Salle incluye Centros educativos, Centros educativos pre-universitarios y de formación profesional, universidades y Obras socioeducativas en España y Portugal, éstas obras se encuentran agrupadas en lo que se llama Coordinadora de Obras Socioeducativas La Salle.</p>
+                    <p>En la actualidad ocho entidades forman parte y otras tres colaboran con ella.</p>
 
                 </div>
             </div>
