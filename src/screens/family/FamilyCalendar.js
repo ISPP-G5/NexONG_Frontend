f2c665273c44ddd78f806903b4502e60d81cf299
manbarjim2 <80253313+manbarjim2@users.noreply.github.com>
2024-04-18 12:40:17 +0200

    fix: event appear

--- src/screens/family/FamilyCalendar.js
+++ src/screens/family/FamilyCalendar.js
@@ -4,26 +4,39 @@ import moment from 'moment';
 import axios from 'axios';
 import '../../styles/styles.css';
 import LayoutProfiles from '../../components/LayoutProfiles';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
+import useToken from '../../components/useToken';
 
 const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT;
 
 const localizer = momentLocalizer(moment);
 
 const FamilyCalendar = () => {
+    const [token, updateToken] = useToken();
+    const config = {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        }
+      };
     const [activities, setActivities] = useState([]);
-    const [studentFamily, setStudentFamily] = useState({
-        studentsId: []
-    });
+    const [studentFamily, setStudentFamily] = useState([]);
     const [currentUser, setCurrentUser] = useState({
         familyId: ''
     });
     const userId = parseInt(localStorage.getItem('userId'), 10);
 
-    const fetchData = async (url, filterFunc) => {
+    const fetchData = async (url, filterFunc = () => true) => {
         try {
-            const response = await axios.get(`${API_ENDPOINT}${url}`);
-            return response.data.filter(filterFunc);
+            const response = await axios.get(`${API_ENDPOINT}${url}`, config);
+            const data = response.data;
+            if (Array.isArray(data)) {
+                return data.filter(filterFunc);
+            } else if (typeof data === 'object') {
+                return filterFunc(data) ? data : null;
+            } else {
+                console.error('Data is not an array or object:', data);
+                return null;
+            }
         } catch (error) {
             console.error(error);
         }
@@ -49,23 +62,35 @@ const FamilyCalendar = () => {
 
     useEffect(() => {
         const fetchAndSetActivities = async () => {
-            const userResponse = await fetchData('auth/users/me/', data => data.family === currentUser.familyId);
+            const userResponse = await fetchData('auth/users/me/');
+
             if (userResponse) {
                 setCurrentUser({ familyId: userResponse.family });
         
                 const studentResponse = await fetchData('student/', student => student.family === currentUser.familyId);
                 if (studentResponse) {
-                    setStudentFamily({ studentsId: studentResponse.id });
+                    setStudentFamily(studentResponse.map(student => student.id));
         
-                    const eventResponse = await fetchData('event/', activity => moment(activity.start_date).isAfter(moment()) && activity.attendees.some(attendee => attendee === studentFamily.studentsId));
+                    const eventResponse = await fetchData('event/', activity => 
+                        moment(activity.start_date).isAfter(moment()) &&
+                        activity.attendees.some(attendee => studentFamily.includes(attendee))
+                    );
                     setActivities(prevActivities => [...prevActivities.filter(event => event.lesson), ...mapActivities(eventResponse, false)]);
-        
-                    const lessonEventResponse = await fetchData('lesson-event/', activity => moment(activity.start_date).isAfter(moment()) && activity.attendees.some(attendee => attendee === studentFamily.studentsId));
+
+                    const lessonEventResponse = await fetchData('lesson-event/', activity => 
+                        moment(activity.start_date).isAfter(moment()) && 
+                        activity.attendees.some(attendee => studentFamily.includes(attendee))
+                    );
                     setActivities(prevActivities => [...prevActivities.filter(event => !event.lesson), ...mapActivities(lessonEventResponse, true)]);
         
-                    const lessonResponse = await fetchData('lesson/', activity => activity.students.some(student => student === studentFamily.studentsId));
-                    setActivities(prevActivities => [...prevActivities.filter(event => !event.lesson), ...mapActivities(lessonResponse, true)]);
+                    const lessonResponse = await fetchData('lesson/', activity => 
+                        activity.students.some(student =>  studentFamily.includes(student))
+                    );
+                    setActivities(prevActivities => [...prevActivities, ...mapActivities(lessonResponse, true)]);
+
                 }
+            }else{
+                toast.error('No se ha podido cargar la información de la familia');
             }
         };
 
