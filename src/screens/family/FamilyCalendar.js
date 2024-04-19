573ab215f08217fadf4a279c709ad90a83da36fa
manbarjim2 <80253313+manbarjim2@users.noreply.github.com>
2024-04-19 12:33:46 +0200

    fix: style

--- src/screens/family/FamilyCalendar.js
+++ src/screens/family/FamilyCalendar.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Calendar, globalizeLocalizer } from 'react-big-calendar';
+import { makeStyles }from '@material-ui/core';
 import moment from 'moment';
 import 'moment/locale/es';
 import Globalize from 'globalize';
@@ -11,6 +12,24 @@ import { ToastContainer, toast } from 'react-toastify';
 import useToken from '../../components/useToken';
 
 const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT;
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      padding: theme.spacing(4),
+    },
+  
+    calendarContainer: {
+      flex: 1,
+      position: 'relative',
+      minHeight: '20rem',
+      marginTop: '2rem',
+      overflow: 'hidden',
+      width: '90%',
+    },
+  }));
 
 const FamilyCalendar = () => {
     Globalize.culture('es');
@@ -26,6 +45,7 @@ const FamilyCalendar = () => {
     const [currentUser, setCurrentUser] = useState({
         familyId: ''
     });
+    const classes = useStyles();
     const userId = parseInt(localStorage.getItem('userId'), 10);
 
     const fetchData = async (url, filterFunc = () => true) => {
@@ -109,13 +129,15 @@ const FamilyCalendar = () => {
     return (
         <LayoutProfiles profile={'familia'} selected={'Calendario'}>
         <ToastContainer />
+        <div className={classes.calendarContainer}>
         <Calendar
             localizer={localizer}
             events={activities}
             startAccessor="start"
             endAccessor="end"
-            className='calendar'            
-        />       
+            className='calendar'     
+        />
+        </div>  
         </LayoutProfiles>
         
     );
