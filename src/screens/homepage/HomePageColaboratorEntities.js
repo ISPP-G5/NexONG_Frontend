f138b9cc01cd42ba5330b391420ae78a7eccf702
claugp06 <99924348+claugp06@users.noreply.github.com>
2024-04-19 13:47:13 +0200

    Fix: feedback pilot users

--- src/screens/homepage/HomePageColaboratorEntities.js
+++ src/screens/homepage/HomePageColaboratorEntities.js
@@ -2,47 +2,53 @@ import '../../styles/styles.css';
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import LayoutHomepage from '../../components/LayoutHomepage';
-
-
+import FundacionLaCaixa from '../../logo/ColaboratorsEntities/+fundacion-la-caixa.jpg';
+import Logotipo_de_la_Junta_de_Andalucia_2020 from '../../logo/ColaboratorsEntities/Logotipo_de_la_Junta_de_Andalucia.png';
+import ayuntamientoSevilla from '../../logo/ColaboratorsEntities/logo_ayuntamiento_sevilla.png';
+import consejeriaIgualdad from  '../../logo/ColaboratorsEntities/Logo_de_la_Consejeria_de_Inclusion_Social_Juventud_Familias_e_Igualdad_de_la_Junta_de_Andalucia.png';
+import misnis from '../../logo/ColaboratorsEntities/misnis2030-150x150.jpg';
+import pablOlavide from  '../../logo/ColaboratorsEntities/pablo-olavide.jpg';
+import consejoSocialUs from  '../../logo/ColaboratorsEntities/consejo-social-us.jpg'
+import fp from  '../../logo/ColaboratorsEntities/ministerioFP.jpg'
 const entidadesData = [
     {
         name:"Junta de andalucia",
-        img:'http://manosabiertasnorte.es/wp-content/uploads/2023/05/1280px-Logotipo_de_la_Junta_de_Andalucia_2020.svg_-300x169.png',
+        img:Logotipo_de_la_Junta_de_Andalucia_2020,
         link:'https://www.juntadeandalucia.es/'
     },
     {
         name:"Ayuntamiento de Sevilla",
-        img:"http://manosabiertasnorte.es/wp-content/uploads/2023/05/logo_ayuntamiento_sevilla.png",
+        img:ayuntamientoSevilla,
         link:'https://www.sevilla.org/servicios/servicios-sociales'
     },
     {
         name:"Fundación La Caixa",
-        img:"http://manosabiertasnorte.es/wp-content/uploads/2023/05/423-4232501_fundacion-la-caixa-300x280.jpg",
+        img:FundacionLaCaixa,
         link:'https://fundacionlacaixa.org/es/programas-sociales'
     },
     {
         name:"Consejería de Inclusión Social, Juventud, Familias e Igualdad J.A.",
-        img:"http://manosabiertasnorte.es/wp-content/uploads/2023/05/Logo_de_la_Consejeria_de_Inclusion_Social_Juventud_Familias_e_Igualdad_de_la_Junta_de_Andalucia.png",
+        img:consejeriaIgualdad,
         link:'https://www.juntadeandalucia.es/organismos/inclusionsocialjuventudfamiliaseigualdad.html'
     },
     {
         name:"Ministerio de derechos sociales y agenda 2030",
-        img:"http://manosabiertasnorte.es/wp-content/uploads/2023/05/misnis2030-150x150.jpg",
+        img:misnis,
         link:'https://www.mdsocialesa2030.gob.es/'
     },
     {
         name:"Universidad Pablo Olavide",
-        img:"http://manosabiertasnorte.es/wp-content/uploads/2023/05/Logos_avales_unis_cuadrados-01-1-150x150.jpg",
+        img:pablOlavide,
         link:'https://www.upo.es/portal/impe/web/portada/index.html'
     },
     {
         name:"Consejo Social Universidad de Sevilla",
-        img:"http://manosabiertasnorte.es/wp-content/uploads/2023/05/LogoCSUS-300x231.jpg",
+        img:consejoSocialUs,
         link:'https://institucional.us.es/consejosocial/'
     },
     {
         name:"Ministerio de Educación y Formación Profesional",
-        img:"http://manosabiertasnorte.es/wp-content/uploads/2023/05/ministerioFP-150x150.jpg",
+        img:fp,
         link:'https://www.educacionyfp.gob.es/portada.html'
     }]
 
