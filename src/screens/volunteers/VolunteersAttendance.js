b7876f3e5dbf1aa148bfaaffbb16d96ab5a497bc
manbarjim2 <80253313+manbarjim2@users.noreply.github.com>
2024-05-04 12:33:50 +0200

    fix: all activities appear correctly

--- src/screens/volunteers/VolunteersAttendance.js
+++ src/screens/volunteers/VolunteersAttendance.js
@@ -10,7 +10,6 @@ import DialogActions from '@material-ui/core/DialogActions';
 import Button from '@material-ui/core/Button';
 import { ToastContainer, toast } from 'react-toastify';
 import useToken from '../../components/useToken';
-import { type } from '@testing-library/user-event/dist/type';
 
 const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT;
 
@@ -21,166 +20,161 @@ const VolunteersAttendance = () => {
       'Authorization': `Bearer ${token}`,
     }
   };
-    const [eventsList, setEventsList] = useState([]);
-    const [currentUser, setCurrentUser] = useState({
-      volunteerId: ''
-    });
-    const [deleteConfirmation, setDeleteConfirmation] = useState({
-      open: false,
-      event: null
+
+  const [eventsList, setEventsList] = useState([]);
+  const [currentUser, setCurrentUser] = useState({
+    volunteerId: ''
   });
-  const userId = parseInt(localStorage.getItem('userId'));
+  const [deleteConfirmation, setDeleteConfirmation] = useState({
+    open: false,
+    event: null
+  });
   const [lessonAttendance, setLessonAttendance] = useState([]);
   const [schedules, setSchedules] = useState([]);
 
   useEffect(() => {
-    axios.get(`${API_ENDPOINT}lesson-attendance/`, config)
-      .then(response => {
-          setLessonAttendance(response.data);
-      }) 
-      .catch (error => {
-          console.error(error);
-      });
-  }, []);
-
-  useEffect(() => {
-    axios.get(`${API_ENDPOINT}auth/users/me/`, config)
-      .then(response => {
-          setCurrentUser({
-            volunteerId: response.data.volunteer
-          });
-      })
-      .catch(error => {
+    const fetchLessonAttendance = async () => {
+      try {
+        const response = await axios.get(`${API_ENDPOINT}lesson-attendance/`, config);
+        setLessonAttendance(response.data);
+      } catch (error) {
         console.error(error);
-      });
-    const fetchAndSetSchedule = async () => {
-      const response = await axios.get(`${API_ENDPOINT}schedule/`, config);
-      if (response) {
-          setSchedules(response.data);
       }
-  };  
-  fetchAndSetSchedule();
+    };
+
+    fetchLessonAttendance();
   }, []);
 
   useEffect(() => {
-    if (lessonAttendance.length > 0) {
-      axios.get(`${API_ENDPOINT}event/`, config)
-        .then(response => {
-          const filteredEvents = response.data.filter(activity => moment(activity.start_date).isAfter(moment()) && 
-                                                                  activity.volunteers.includes(currentUser.volunteerId));
-          setEventsList(filteredEvents.map(activity => ({
-            name: activity.name,
-            start_date: new Date(activity.start_date),
-            end_date: new Date(activity.end_date),
-            id: activity.id,
-            description: activity.description,
-            place: activity.place,
-            max_volunteers: activity.max_volunteers,
-            max_attendees: activity.max_attendees,
-            price: activity.price,
-            attendees: activity.attendees,
-            volunteers: activity.volunteers,
-            url: activity.url,
-            type: 'event'
-          })));
-          })
-          .catch(error => {
-            console.error(error);
-          });
-
-          axios.get(`${API_ENDPOINT}lesson-event/`, config)
-          .then(response => {
-            const filteredEvents = response.data.filter(activity => moment(activity.start_date).isAfter(moment()) &&
-                                                                    activity.volunteers.includes(currentUser.volunteerId));
-            setEventsList(prevEvents => [...prevEvents, ...filteredEvents.map(activity => ({
-              id: activity.id,
-              name: activity.name,
-              description: activity.description,
-              place: activity.place,
-              max_volunteers: activity.max_volunteers,
-              start_date: new Date(activity.start_date),
-              end_date: new Date(activity.end_date),          
-              lesson: activity.lesson,
-              price: activity.price,
-              educators: activity.educators,
-              attendees: activity.attendees,
-              volunteers: activity.volunteers,
-              url: activity.url,
-              type: 'lesson-event'
-            }))]);
-          })
-          .catch(error => {
-            console.error(error);
-          } );
-
-          axios.get(`${API_ENDPOINT}lesson/`, config)
-          .then(response => {
-            const filteredEvents = response.data.filter(activity => moment(activity.end_date).isAfter(moment()) &&
-            lessonAttendance.some(attendance => attendance.lesson === activity.id && attendance.volunteer === currentUser.volunteerId));
-            setEventsList(prevEvents => [...prevEvents, ...filteredEvents.map(activity => ({
-              id: activity.id,
-              name: activity.name,
-              description: activity.description,
-              capacity: activity.capacity,
-              is_morning_lesson: activity.is_morning_lesson,
-              start_date: new Date(activity.start_date),
-              end_date: new Date(activity.end_date),          
-              educator: activity.educator,
-              students: activity.students,
-              url: activity.url,
-              type: 'lesson'
-            }))]);
-          })
-          .catch(error => {
-            console.error(error);
-          } );
+    const fetchCurrentUser = async () => {
+      try {
+        const response = await axios.get(`${API_ENDPOINT}auth/users/me/`, config);
+        setCurrentUser({
+          volunteerId: response.data.volunteer
+        });
+      } catch (error) {
+        console.error(error);
       }
-    }, [userId,currentUser.volunteerId]);
+    };
 
-    const handleDeleteConfirmation = (event) => {
-      setDeleteConfirmation({
-          open: true,
-          event: event
-      });
+    fetchCurrentUser();
+  }, []);
+
+  useEffect(() => {
+    const fetchSchedules = async () => {
+      try {
+        const response = await axios.get(`${API_ENDPOINT}schedule/`, config);
+        setSchedules(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchSchedules();
+  }, []);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const eventResponses = await Promise.all([
+          axios.get(`${API_ENDPOINT}event/`, config),
+          axios.get(`${API_ENDPOINT}lesson-event/`, config)
+        ]);
+
+        const eventsData = eventResponses.reduce((acc, response) => {
+          acc.push(...response.data);
+          return acc;
+        }, []);
+
+        const filteredEvents = eventsData.filter(activity =>
+          moment(activity.start_date).isAfter(moment()) &&
+          activity.volunteers.includes(currentUser.volunteerId)
+        );
+
+        setEventsList(filteredEvents.map(activity => ({
+          ...activity,
+          start_date: new Date(activity.start_date),
+          end_date: new Date(activity.end_date),
+          type: activity.lesson ? 'lesson-event' : 'event'
+        })));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchEvents();
+  }, [currentUser.volunteerId]);
+
+  useEffect(() => {
+    const fetchLessons = async () => {
+      try {
+        const eventResponses = await Promise.all([
+          axios.get(`${API_ENDPOINT}lesson/`, config)
+        ]);
+
+        const eventsData = eventResponses.reduce((acc, response) => {
+          acc.push(...response.data);
+          return acc;
+        }, []);
+
+        const filteredLessons = eventsData.filter(activity =>
+          moment(activity.end_date).isAfter(moment()) &&
+          lessonAttendance.some(attendance => attendance.lesson === activity.id && attendance.volunteer === currentUser.volunteerId)
+        );
+        setEventsList(preEvents => [
+          ...preEvents,
+          ...filteredLessons.map(activity => ({
+          ...activity,
+          start_date: new Date(activity.start_date),
+          end_date: new Date(activity.end_date),
+          type: 'lesson'
+        }))
+      ]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchLessons();
+  }, [lessonAttendance]);
+
+  const handleDeleteConfirmation = (event) => {
+    setDeleteConfirmation({
+      open: true,
+      event: event
+    });
   };
 
-  const handleDeleteVolunteer = () => {
-    if (deleteConfirmation.event.type === 'lesson-event' || deleteConfirmation.event.type === 'event') {
-      const updatedVolunteers = deleteConfirmation.event.volunteers.filter(volunteer => volunteer !== currentUser.volunteerId);
-      axios.put(`${API_ENDPOINT}${deleteConfirmation.event.lesson ? 'lesson-event' : 'event'}/${deleteConfirmation.event.id}/`, { ...deleteConfirmation.event, volunteers: updatedVolunteers }, config)
-          .then(response => {
-              toast.success('Se ha eliminado correctamente');
-              setDeleteConfirmation({
-                  open: false,
-                  event: null
-              });
-              setTimeout(() => {
-                  window.location.reload();
-              }, 1000);
-          })
-          .catch(error => {
-              console.error('Error al eliminar el voluntario:', error);
-              toast.error('Error al elimninar el voluntario')
-              setDeleteConfirmation({ open: false, event: null })
-          });
-    }else if (deleteConfirmation.event.type === 'lesson') {
-      const attendanceId = lessonAttendance.find(attendance => attendance.lesson === deleteConfirmation.event.id && attendance.volunteer === currentUser.volunteerId).id;
-      axios.delete(`${API_ENDPOINT}lesson-attendance/${attendanceId}`, config)
-          .then(response => {
-              toast.success('Se ha eliminado correctamente');
-              setDeleteConfirmation({
-                  open: false,
-                  event: null
-              });
-              setTimeout(() => {
-                  window.location.reload();
-              }, 1000);
-          })
-          .catch(error => {
-              console.error('Error al eliminar el voluntario:', error);
-              toast.error('Error al elimninar el voluntario')
-              setDeleteConfirmation({ open: false, event: null })
-          });
+  const handleDeleteVolunteer = async () => {
+    try {
+      let url = `${API_ENDPOINT}${deleteConfirmation.event.lesson ? 'lesson-event' : 'event'}/${deleteConfirmation.event.id}/`;
+      let data = { ...deleteConfirmation.event };
+
+      if (deleteConfirmation.event.type === 'lesson') {
+        const attendanceId = lessonAttendance.find(attendance =>
+          attendance.lesson === deleteConfirmation.event.id && attendance.volunteer === currentUser.volunteerId
+        ).id;
+        url = `${API_ENDPOINT}lesson-attendance/${attendanceId}`;
+      } else {
+        const updatedVolunteers = deleteConfirmation.event.volunteers.filter(volunteer =>
+          volunteer !== currentUser.volunteerId
+        );
+        data.volunteers = updatedVolunteers;
+      }
+
+      const response = await axios.put(url, data, config);
+      toast.success('Se ha eliminado correctamente');
+      setDeleteConfirmation({
+        open: false,
+        event: null
+      });
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
+    } catch (error) {
+      console.error('Error al eliminar el voluntario:', error);
+      toast.error('Error al eliminar el voluntario');
+      setDeleteConfirmation({ open: false, event: null });
     }
   };
 
@@ -188,12 +182,12 @@ const VolunteersAttendance = () => {
     <LayoutProfiles profile={'voluntario'} selected={'Asistencia'}>
       <ToastContainer/>
       {eventsList.length === 0 ? (
-        <div className= "centered-message">
-        <p>No te has unido a ningún evento aún</p>
+        <div className="centered-message">
+          <p>No te has unido a ningún evento aún</p>
         </div>
       ) : (
         eventsList.map((event) => (
-          <div className='card-info' key={event.id}>
+          <div className='card-info' key={event.id + event.name}>
             <div>
               <p><strong>Evento:</strong> {event.name}</p>
               <p><strong>Tipo:</strong> {event.type === 'event' ? 'Evento' : event.type === 'lesson' ? 'Clase' : 'Excursión'}</p>
@@ -206,13 +200,13 @@ const VolunteersAttendance = () => {
                 (<div>
                   <p><strong>Comienzo: </strong>{event.start_date.getDate()}/{event.start_date.getMonth()+1}/{event.start_date.getFullYear()}</p>
                   <p><strong>Final: </strong>{event.end_date.getDate()}/{event.end_date.getMonth()+1}/{event.end_date.getFullYear()}</p>
-                  <p><strong>Horario:</strong> {
+                  <div><p><strong>Horario:</strong></p> {
                     schedules.filter(schedule => schedule.lesson === event.id).map(schedule => 
-                    <p>{schedule.weekday} de {schedule.start_time} a {schedule.end_time}</p>
+                      <p>{schedule.weekday} de {schedule.start_time} a {schedule.end_time}</p>
                     )
-                  }</p>
+                  }</div>
                 </div>) 
-                }
+              }
             </div>
             <div className='edit-delete-icons'>
               <DeleteIcon className='trash' onClick={() => handleDeleteConfirmation(event)} />
@@ -220,7 +214,7 @@ const VolunteersAttendance = () => {
           </div>
         ))
       )}
-  
+
       <Dialog open={deleteConfirmation.open} onClose={() => setDeleteConfirmation({ open: false, event: null })}>
         <DialogTitle>¿Quieres eliminar tu participación en este evento?</DialogTitle>
         <DialogActions>
